662d5f0265ca67b06399ef08054dc9ec714697de
Tom Thavee <tomthavee@Toms-MBP.fios-router.home>
2018-12-15 03:26:39 -0500

    Initial version

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { initializeIcons } from '@uifabric/icons';
+import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
+import * as React from 'react';
+import './App.css';
+import logo from './logo.svg';
+
+initializeIcons();
+
+// ...or, register icons and pull the fonts from your own CDN:
+initializeIcons('https://my.cdn.com/path/to/icons/');
+
+class App extends React.Component {
+  public render() {
+    const name = 'Wai-Ing';
+    const element = <h1>Hello, {name}</h1>;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <DefaultButton
+          text='See Button'
+          primary={ true }
+          href='#/components/button'
+        />
+        <h1> {element} </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
